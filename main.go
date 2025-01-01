5b1dfeb97d7c5d58f2677016d457579af935794e
jjhonny <ojhonny1@hotmail.com>
2025-01-01 17:14:10 -0300

    Add initial implementation of HTTP server with custom handler

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+func (MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(201)
+	w.Write([]byte("Olá, mundo!!"))
+}
+
+func main() {
+	fmt.Println("Servidor rodando na porta 5000")
+	handler := MyHandler{}
+	http.ListenAndServe(":5000", handler)
+}
