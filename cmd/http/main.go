e1560ac65ee72b778da86dc46eedb99aeaa44788
jjhonny <ojhonny1@hotmail.com>
2025-01-16 11:26:20 -0400

    Refactor HTTP handlers into a separate package for improved organization and maintainability

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -2,78 +2,12 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/jjhonny/quicknotes/internal/handlers"
 )
-
-func noteList(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	files := []string{
-		"views/templates/base.html",
-		"views/templates/pages/home.html",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
-		return
-	}
-	slog.Info("Executou o handler /")
-	t.ExecuteTemplate(w, "base", nil)
-}
-
-func noteView(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		http.Error(w, "Nota não encontrada.", http.StatusNotFound)
-		return
-	}
-
-	files := []string{
-		"views/templates/base.html",
-		"views/templates/pages/note-view.html",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
-		return
-	}
-
-	t.ExecuteTemplate(w, "base", id)
-}
-
-func noteNew(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.Header().Set("Allow", http.MethodGet)
-		http.Error(w, "Método não permitido.", http.StatusMethodNotAllowed)
-		return
-	}
-
-	files := []string{
-		"views/templates/base.html",
-		"views/templates/pages/note-new.html",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		http.Error(w, "Aconteceu um erro ao executar essa página", http.StatusInternalServerError)
-		return
-	}
-
-	t.ExecuteTemplate(w, "base", nil)
-}
-
-func noteCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Método não permitido.", http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Fprint(w, "Criando uma nova nota...")
-}
 
 func main() {
 	config := loadConfig()
@@ -88,10 +22,12 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix("/static", staticHandler))
 
-	mux.HandleFunc("/", noteList)
-	mux.HandleFunc("/note/view", noteView)
-	mux.HandleFunc("/note/new", noteNew)
-	mux.HandleFunc("/note/create", noteCreate)
+	noteHandler := handlers.NewNoteHandler()
+
+	mux.HandleFunc("/", noteHandler.NoteList)
+	mux.HandleFunc("/note/view", noteHandler.NoteView)
+	mux.HandleFunc("/note/new", noteHandler.NoteNew)
+	mux.HandleFunc("/note/create", noteHandler.NoteCreate)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", config.ServerPort), mux)
 }
