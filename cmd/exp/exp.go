aad07432f15dadca910ed89a594bfd4974ffc822
jjhonny <ojhonny1@hotmail.com>
2025-01-16 10:37:58 -0400

    Refactor logging implementation to use slog package and remove JSON configuration loading

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,28 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"log/slog"
 	"os"
 )
 
-type Config struct {
-	Server struct {
-		Port      int
-		Host      string
-		StaticDir string
-	}
-}
-
+// arquive for examples
 func main() {
-	file, err := os.Open("config.json")
-	if err != nil {
-		panic(err)
-	}
-	var config Config
-	err = json.NewDecoder(file).Decode(&config)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Dir Static: %s\n %s%d", config.Server.StaticDir, config.Server.Host, config.Server.Port)
+	// h := slog.NewTextHandler(os.Stderr, nil)
+	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+
+	log := slog.New(h).With("app", "exp")
+
+	log.Debug("Debug message")
+	log.Info("Info message", slog.Group("request_info", "request_id", 123))
+	log.Warn("warn message")
+	log.Error("error message", "request_id", 123)
 }
